17cf61d82c0093cad78e0c27092137de709402b8
IssEdgarH <ehernandezheredia@csumb.edu>
2023-10-03 21:56:59 -0700

    Replaced progress code with solution code

--- src/components/AdminHome.js
+++ src/components/AdminHome.js
@@ -1,51 +1,84 @@
 import React, { useState, useEffect } from 'react';
+import AddStudent from './AddStudent';
+import EditStudent from './EditStudent';
+import {SERVER_URL} from '../constants';
 
 const AdminHome = ()  => {
 
-  const [id, setId] = useState('');
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [status, setStatus] = useState('');
+    useEffect(() => {
+        // called once after intial render
+        fetchStudents();
+        }, [] )
 
-  useEffect(() => {
-    // called once after intial render
-    fetchStudents();
-  }, [] )
+    /* 
+        *  list of students 
+    */
+    const [students, setStudents] = useState([]);
+    const [message, setMessage] = useState('');
 
+    const fetchStudents = () => {
+        console.log("fetchStudents ");
+        fetch(`${SERVER_URL}/student`)
+        .then((response) => response.json() )
+        .then((data) => setStudents(data) )
+        .catch((err) =>  { console.log("fetch error "+err); } );
+    }
 
-  const fetchStudents = () => {
-    //TODO complete this method to fetch students and display list of students
-    fetch('http://localhost:8080/student')
-    .then(response => response.json())
-    .then(data => {setFactors({id:data.student_id, name:data.name, email:data.email, status:data.status});})
-    .catch(err => console.log(err));
-  }
+    const refreshStudents = () => {
+        setMessage('');
+        fetchStudents();
+    }
 
-  const addStudent = (e) => {
-    props.postAttempt(attempt, alias);
-  }
+    const deleteStudent = (event) => {
+        const row_id = event.target.parentNode.parentNode.rowIndex - 1;
+        console.log("deleteStudent "+row_id);
+        const studentId = students[row_id].studentId;
+        console.log("student_id "+studentId);
+        fetch(`${SERVER_URL}/student/${studentId}`, 
+          {  
+            method: 'DELETE', 
+          } 
+        )
+        .then((response) => { 
+            if (response.ok) {
+                setMessage('Student deleted.');
+                fetchStudents();
+            }
+         } )
+        .catch((err) =>  { setMessage('Error. '+err) } );
+    }
 
-  return (
-    <div>
-      <div margin="auto">
-        <h3>Student List</h3>
+    const headers = ['ID', 'name', 'email', 'status code', 'reason',' ', ' '];
+
+    return (
+        <div> 
+        <div margin="auto" >
+          <h3>Students</h3>
+          <h4>{message}</h4>
+            <table className="Center"> 
+              <thead>
+                <tr>
+                  {headers.map((s, idx) => (<th key={idx}>{s}</th>))}
+                </tr>
+              </thead>
+              <tbody>
+              {students.map((row,idx) => (
+                      <tr key={idx}>
+                        <td>{row.studentId}</td>
+                        <td>{row.name}</td>
+                        <td>{row.email}</td>
+                        <td>{row.statusCode}</td>
+                        <td>{row.status}</td>
+                        <td><EditStudent data={students} onClose={refreshStudents} /></td>
+                        <td><button type="button" margin="auto" onClick={deleteStudent}>Delete</button></td>
+                      </tr>
+                    ))}
+              </tbody>
+            </table>
+            <AddStudent onClose={refreshStudents} />
+        </div>
       </div>
-      <table>
-        <tbody>
-          <tr><label> ID </label></tr>
-          <tr><label> Name </label></tr>
-          <tr><label> Email </label></tr>
-          <tr><label> Status Code </label></tr>
-          <tr><label> Status </label></tr>
-          <tr><label> Delete </label></tr>
-          <tr><label> Update </label></tr>
-        </tbody>
-      </table>
-
-      <br/>
-      <button onClick={addStudent}>Add</button>
-    </div>
-  )
+    )
 }
 
 export default AdminHome;
